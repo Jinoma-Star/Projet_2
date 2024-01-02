399f9bc34434641b57d046ec5b03aaf3c5900a6f
Jinoma-Star <antoine.rami@hotmail.fr>
2024-01-02 19:03:28 +0100

    Etape 1

--- Backend/projects.js
+++ Backend/projects.js
@@ -1,14 +1,28 @@
-fetch('http://localhost:5678/api/works')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    // Traitez les données ici
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+function miseAJourGalerie(travaux) {
+    const galerie = document.querySelector('.gallery'); // Sélectionne le premier élément avec la classe 'gallery'
+    galerie.innerHTML = ''; // Nettoyer le contenu existant
+  
+    travaux.forEach(travail => {
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      img.src = travail.imageUrl;
+      img.alt = travail.title;
+  
+      const figcaption = document.createElement('figcaption');
+      figcaption.textContent = travail.title;
+  
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      galerie.appendChild(figure);
+    });
+  }
+  
+  fetch('http://localhost:5678/api/works')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(travaux => miseAJourGalerie(travaux))
+    .catch(error => console.error('There has been a problem with your fetch operation:', error));
